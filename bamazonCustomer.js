2a11750da20c86731bd44873d7521262394cf0cd
samlutz <sammlutz@gmail.com>
2019-04-23 15:50:59 -0400

    first inquirer prompts functional

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,20 +18,22 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    addProduct("Kingdom Hearts 3", "Video Games", 40.00, 50);
-    addProduct("Bamazon Balexa", "Homegoods", 150.00, 25);
-    addProduct("Headphones", "Electronics", 25.00, 20);
-    addProduct("Wireless Mouse", "Electronics", 50.00, 30);
-    addProduct("Game System", "Electronics", 400.00, 10);
-    addProduct("Toothbrush", "Homegoods", 1.00, 500);
-    addProduct("Bananas", "Grocery", .50, 150);
-    addProduct("Apples", "Grocery", .75, 100);
-    addProduct("Crockpot", "Homegoods", 30.00, 35);
-    addProduct("TV", "Electronics", 300.00, 60);
-    addProduct("Frozen Pizza", "Grocery", 8.00, 120);
+    start();
+    // placeOrder();
+    // addProduct("Kingdom Hearts 3", "Video Games", 40.00, 50);
+    // addProduct("Bamazon Balexa", "Homegoods", 150.00, 25);
+    // addProduct("Headphones", "Electronics", 25.00, 20);
+    // addProduct("Wireless Mouse", "Electronics", 50.00, 30);
+    // addProduct("Game System", "Electronics", 400.00, 10);
+    // addProduct("Toothbrush", "Homegoods", 1.00, 500);
+    // addProduct("Bananas", "Grocery", .50, 150);
+    // addProduct("Apples", "Grocery", .75, 100);
+    // addProduct("Crockpot", "Homegoods", 30.00, 35);
+    // addProduct("TV", "Electronics", 300.00, 60);
+    // addProduct("Frozen Pizza", "Grocery", 8.00, 120);
 
     // deleteProduct("Bamazon Balexa");
-    getAll();
+    // getAll();
     connection.end();
 
 });
@@ -78,3 +80,28 @@ const deleteProduct = function (item) {
         })
 };
 
+// start function to initiate buyer experience
+function start() {
+    connection.query("SELECT * FROM Bamazon_inventory", function (err, res) {
+        if (err) throw err;
+        console.table(res)
+        // connection.end();
+        placeOrder();
+    })
+}
+
+function placeOrder() {
+    inquirer
+        .prompt([
+            {
+                name: "order",
+                type: "input",
+                message: "Select item for purchase by entering the ID"
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: "How many units of this product would you like to buy?"
+            }
+        ])
+}
